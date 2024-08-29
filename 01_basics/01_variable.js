0f41483170214c410853093b094a3754398c9fef
SourabhDangwal <sourabhlink123@gmail.com>
2024-08-29 13:12:13 +0530

    Add files via upload

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const acountId = 14453 // remains constant cant change its value
+let acountEmail = "abc@google.com"
+var acountPassword = "12345"
+acountCity = "manali"
+
+// acountId = 2 // gives error
+console.log(acountId);
+acountEmail = "om@google.com"
+acountPassword = "1111"
+acountCity = "Bengaluru"
+console.table([acountId,acountEmail,acountPassword,acountCity])
